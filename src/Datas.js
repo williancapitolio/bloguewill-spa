5874d2c5f8c94817a8547e062bcbc3e45c525dc6
Willian <williansilvap25@gmail.com>
2023-01-19 22:03:57 -0300

    create data mock

---
+++ src/Datas.js
@@ -0,0 +1,30 @@
+export const news = [
+    {
+        title: "Notícia 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non quam sed elit placerat blandit. Phasellus convallis dolor nisl, nec molestie enim blandit non. Donec luctus semper cursus. Fusce nec tortor lobortis, rutrum libero sit amet, hendrerit felis. Mauris consequat ut ipsum a dignissim. Aliquam eget augue tellus. Aliquam at viverra dolor.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9Ah0CnwyoNAliZ6Aoa8GAH1HTLHilwh07Sg&usqp=CAU",
+        likes: "3",
+        comments: "1"
+    },
+    {
+        title: "Notícia 2",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non quam sed elit placerat blandit. Phasellus convallis dolor nisl, nec molestie enim blandit non. Donec luctus semper cursus. Fusce nec tortor lobortis, rutrum libero sit amet, hendrerit felis. Mauris consequat ut ipsum a dignissim. Aliquam eget augue tellus. Aliquam at viverra dolor.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgcppzbsLmXtCvNhNdB3LHK0RDcmdYyKjXtg&usqp=CAU",
+        likes: "4",
+        comments: "3"
+    },
+    {
+        title: "Notícia 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non quam sed elit placerat blandit. Phasellus convallis dolor nisl, nec molestie enim blandit non. Donec luctus semper cursus. Fusce nec tortor lobortis, rutrum libero sit amet, hendrerit felis. Mauris consequat ut ipsum a dignissim. Aliquam eget augue tellus. Aliquam at viverra dolor.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1LSID7OsLuwVx7w0MaFiksKeIXPZTLqic_w&usqp=CAU",
+        likes: "2",
+        comments: "2"
+    },
+    {
+        title: "Notícia 4",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non quam sed elit placerat blandit. Phasellus convallis dolor nisl, nec molestie enim blandit non. Donec luctus semper cursus. Fusce nec tortor lobortis, rutrum libero sit amet, hendrerit felis. Mauris consequat ut ipsum a dignissim. Aliquam eget augue tellus. Aliquam at viverra dolor.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRxzJdPcuXQuLbS2KW6yJlPwHcJFovSIK17Q&usqp=CAU",
+        likes: "5",
+        comments: "0"
+    }
+];
